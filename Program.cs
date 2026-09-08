3ce53d64d9d1c1d13f41fcf0af8a6d21c347aa0f
Letícia Soares <leticias.colman@gmail.com>
2026-09-08 15:26:25 -0400

    implementação da classe DAO passo 8

--- Program.cs
+++ Program.cs
@@ -1,23 +1,23 @@
 using AppWebLeticia.Components;
+using AppWebLeticia.Configs;
+using AppWebLeticia.DAO;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+// Configuração da Conexão com o Banco de Dados MySQL
+builder.Services.AddScoped<Conexao>();
+builder.Services.AddScoped<ProcessoDAO>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
 }
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
-app.UseHttpsRedirection();
-
 app.UseAntiforgery();
 
 app.MapStaticAssets();
